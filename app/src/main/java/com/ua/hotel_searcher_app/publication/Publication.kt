bc9fa33eb4826501ee1763fca05a04a784f1deb5
n-mangini <nicolas.mangini@ing.austral.edu.ar>
2024-08-21 23:33:30 -0300

    feat: add images

--- app/src/main/java/com/ua/hotel_searcher_app/publication/Publication.kt
+++ app/src/main/java/com/ua/hotel_searcher_app/publication/Publication.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -20,24 +23,30 @@ fun Publication(publicationItem: PublicationItem) {
             .fillMaxWidth()
             .padding(16.dp)
     ) {
+        Image(
+            painter = painterResource(id = publicationItem.imageResId),
+            contentDescription = publicationItem.title,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .clip(RoundedCornerShape(8.dp))
+        )
+
         Text(
             text = publicationItem.title,
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(8.dp)
         )
-
-/*        Image(
-            painter = painterResource(id = publicationItem.imageResId),
-            contentDescription = publicationItem.title,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-        )*/
 
         Text(
             text = publicationItem.description,
             fontSize = 16.sp
+        )
+
+        Text(
+            text = publicationItem.price,
+            fontSize = 16.sp,
         )
     }
 }
--- app/src/main/java/com/ua/hotel_searcher_app/publication/PublicationItem.kt
+++ app/src/main/java/com/ua/hotel_searcher_app/publication/PublicationItem.kt
@@ -1,25 +1,35 @@
 package com.ua.hotel_searcher_app.publication
 
+import com.ua.hotel_searcher_app.R
+
 data class PublicationItem(
     val title: String,
     val description: String,
-    val location: String
+    val location: String,
+    val price: String,
+    val imageResId: Int
 )
 
 val publications = listOf(
     PublicationItem(
         "Cozy Apartment",
         "A beautiful place to stay in the city center.",
-        "New York"
+        "New York",
+        "$120/night",
+        imageResId = R.drawable.image_1
     ),
     PublicationItem(
         "Modern Loft",
         "Spacious and bright loft with modern amenities.",
-        "Los Angeles"
+        "Los Angeles",
+        "$200/night",
+        imageResId = R.drawable.image_1
     ),
     PublicationItem(
         "Beach House",
         "Enjoy the sea breeze at this beachfront property.",
-        "Zarate, Argentina"
+        "Zarate, Argentina",
+        "$1/night",
+        imageResId = R.drawable.image_1
     )
 )
