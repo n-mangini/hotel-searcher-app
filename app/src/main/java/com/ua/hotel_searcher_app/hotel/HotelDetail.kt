a26b662a240defa09fdf8cab0e010ac2de258865
n-mangini <nicolas.mangini@ing.austral.edu.ar>
2024-09-27 20:14:21 -0300

    refactor: simplify Hotel composables

--- app/src/main/java/com/ua/hotel_searcher_app/hotel/HotelDetail.kt
+++ app/src/main/java/com/ua/hotel_searcher_app/hotel/HotelDetail.kt
@@ -1,9 +1,7 @@
 package com.ua.hotel_searcher_app.hotel
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -11,22 +9,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.ua.hotel_searcher_app.R
+import coil.compose.AsyncImage
 
 @Composable
 fun HotelDetail(hotel: HotelModel) {
     Column(modifier = Modifier.padding(16.dp)) {
-        Image(
-            painter = painterResource(R.drawable.image_1),
+        AsyncImage(
+            model = hotel.imgUrl,
             contentDescription = hotel.title,
-            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp)
                 .clip(RoundedCornerShape(6.dp))
         )
         Text(
@@ -41,7 +35,7 @@ fun HotelDetail(hotel: HotelModel) {
             modifier = Modifier.padding(vertical = 4.dp)
         )
         Text(
-            text = "$${hotel.price}",
+            text = hotel.price,
             style = MaterialTheme.typography.titleMedium
         )
     }
