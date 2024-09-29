e5154f53190d53aca2adee094dea42dda7ccefe1
n-mangini <nicolas.mangini@ing.austral.edu.ar>
2024-09-29 17:49:14 -0300

    feat: extracted resources -> profile

--- app/src/main/java/com/ua/innVista/hotel/HotelList.kt
+++ app/src/main/java/com/ua/innVista/hotel/HotelList.kt
@@ -40,6 +40,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
@@ -254,16 +255,3 @@ fun RetryView(viewModel: HotelListViewModel) {
     }
 }
 
-/*@Preview
-@Composable
-fun PreviewHotelView() {
-    HotelView(
-        hotel = HotelModel(
-            "Wyndham Garden Campana",
-            "60700",
-            "Campana, Buenos Aires",
-            "Alojamiento informal con restaurante y spa",
-            ""
-        ), wishlistViewModel = WishlistViewModel()
-    ) {}
-}*/
--- app/src/main/java/com/ua/innVista/profile/Profile.kt
+++ app/src/main/java/com/ua/innVista/profile/Profile.kt
@@ -1,6 +1,5 @@
 package com.ua.innVista.profile
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,9 +8,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
@@ -24,8 +25,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,95 +39,113 @@ fun Profile() {
     val viewModel = hiltViewModel<ProfileViewModel>()
     val userName by viewModel.userName.collectAsState()
 
-    var userNameLocal by remember {
-        mutableStateOf("")
-    }
-
-    var isToggled by remember { mutableStateOf(false) }
-
-    //TODO extract methods
     if (userName.isEmpty()) {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text(
-                text = "Elegir nombre de usuario",
-                fontWeight = FontWeight.Bold,
-            )
-
-            TextField(
-                value = userNameLocal,
-                onValueChange = { userNameLocal = it },
-                label = {
-                    Text(text = "Username")
-                },
-            )
-
-            Button(
-                onClick = { viewModel.saveToDataStore(userNameLocal) },
-                colors = ButtonDefaults.buttonColors(
-                    //TODO extract colours
-                    containerColor = Color(0xff349aa6),
-                    contentColor = Color.Black
-                )
-            ) {
-                Text(text = "save")
-            }
-        }
+        SetUsername(
+            onSave = { viewModel.saveToDataStore(it) }
+        )
     } else {
+        UserProfile(
+            userName = userName
+        )
+    }
+}
 
-        Column(
-            modifier = Modifier
-                .padding(16.dp)
+@Composable
+fun SetUsername(
+    onSave: (String) -> Unit
+) {
+    var userNameLocal by remember { mutableStateOf("") }
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = stringResource(R.string.ask_user_name),
+            fontWeight = FontWeight.Bold,
+        )
+
+        TextField(
+            value = userNameLocal,
+            onValueChange = { userNameLocal = it },
+            label = {
+                Text(text = stringResource(R.string.username))
+            },
+        )
+
+        Button(
+            onClick = { onSave(userNameLocal) },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(R.color.appBlue),
+                contentColor = colorResource(R.color.black)
+            )
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.baseline_person_24),
-                    contentDescription = "Profile Icon",
-                    modifier = Modifier.size(150.dp)
-                )
-
-                Spacer(modifier = Modifier.width(16.dp))
-
-                Text(
-                    text = viewModel.userName.value,
-                    fontSize = 32.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.weight(1f)
-                )
-            }
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "Dark Mode",
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Medium,
-                    modifier = Modifier.weight(1f)
-                )
-
-                Switch(
-                    checked = isToggled,
-                    onCheckedChange = { isToggled = it },
-                    colors = SwitchDefaults.colors(
-                        checkedThumbColor = MaterialTheme.colorScheme.primary,
-                        uncheckedThumbColor = MaterialTheme.colorScheme.onBackground
-                    )
-                )
-            }
+            Text(text = stringResource(R.string.save))
         }
     }
 }
 
 @Composable
-@Preview
-fun PreviewProfile() {
-    Profile()
+fun UserProfile(userName: String) {
+    var isToggled by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = stringResource(R.string.profile_icon),
+                modifier = Modifier.size(150.dp)
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Text(
+                text = userName,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(R.string.dark_mode),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.weight(1f)
+            )
+
+            Switch(
+                checked = isToggled,
+                onCheckedChange = { isToggled = it },
+                colors = SwitchDefaults.colors(
+                    checkedThumbColor = colorResource(R.color.appBlue),
+                    uncheckedThumbColor = colorResource(R.color.white),
+                    checkedTrackColor = colorResource(R.color.appBlueLight),
+                    uncheckedTrackColor = colorResource(R.color.gray)
+                )
+            )
+        }
+    }
 }
 
+
+@Composable
+@Preview
+fun PreviewProfile() {
+    UserProfile(userName = "Marcelo")
+}
+
+@Composable
+@Preview
+fun PreviewSetUsername() {
+    SetUsername {}
+}
